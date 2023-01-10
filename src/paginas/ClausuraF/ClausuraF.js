22f1cee1628b465f410152be30b66ffc2125c2d1
AlvarezMariaBelen <beluchis1991@gmail.com>
2023-01-10 18:25:39 -0300

    finalizado de secciones

--- src/paginas/ClausuraF/ClausuraF.js
+++ src/paginas/ClausuraF/ClausuraF.js
@@ -12,9 +12,9 @@ export default function ClausuraF(){
             <div className="lugarCf">
                 <h3>¿Dondé?</h3>
                 <img className="logoCf2" src="./1america.png"></img>
-                <button className="botonCf"><img src="./ubicacion.png"></img>Ubicación Google Maps</button>
+                <button className="botonCf">Ubicación Google Maps</button>
                 <p>Categoría Libre Femenino <br></br> Domingos por la tarde</p>
-                <h4>Promociones especiales para equipos que participen tambien en alguna categoría Masculino CR</h4>
+                <h4>¡Promociones especiales para equipos que participen tambien en alguna categoría Masculino CR!</h4>
             </div>
             <div className="experienciaCf">
                 <h2>EXPERIENCIA JAUS COPA RELÁMPAGO</h2>
@@ -76,6 +76,52 @@ export default function ClausuraF(){
             <p>Afiliad@s a la Fédératione Internationale de <br></br> Football 7 y a la Asociación Argentina de Fútbol 7.</p>
             <p>El único Seleccionado Femenino en Córdoba <br></br>que compite en las máximas competencias nacionales. ¿Estan listas?</p>
             <img src="./AF7A-y-FIF7.png"></img>
+          </div>
+          <div className="requisitosCf">
+            <h2>REQUISITOS E INSCRIPCIÓN</h2>
+            <div className="seccionRequisitosPrincipalCf">
+                <div className="seccionRequisitosCf ">
+                    <img src="./pago.png"></img>
+                    <h3>INSCRIPCIÓN</h3>
+                    <p>Al cumplirse la fecha 02 de competición deberá estar abonado el %100 de la inscripción, de lo contrario el equipo podrá ser expulsado sin derecho a reclamo.</p>
+                </div>
+                <div className="seccionRequisitosCf">
+                    <img src="./camiseta.png"></img>
+                    <h3>CAMISETAS ENUMERADAS</h3>
+                    <ul className="listaCamisetasCf">
+                        <li><span>LIBRE:</span> obligatorio</li>
+                        <li><span>SUB23:</span> obligatorio</li>
+                        <li><span>SUB20:</span> obligatorio</li>
+                        <li><span>SUB18:</span> obligatorio</li>
+                        <li><span>SUB16:</span> obligatorio</li>
+                        <li><span>FEMENINO:</span> obligatorio</li>
+                    </ul>
+                </div>
+                <div className="seccionRequisitosCf">
+                    <img src="./leyendo.png"></img>
+                    <h3>REGLAMENTO</h3>
+                    <p>En JAUS by CopaRelampago, lo fundamental es la disciplina. No se toleran faltas de respeto de ningún índole. Todos los equipos deben aceptar el reglamento para ser aceptados.</p>
+                </div>
+            </div>
+          </div>
+          <div className="seccionPasosCf">
+            <div className="pasosCf">
+                <img src="./globos-de-texto.png"></img>
+                <h3>COMUNICARSE</h3>
+                <p>Primero, comunicate con nosotros así te sacás todas las dudas, y te explicamos como inscribir tu equipo.</p>
+                <button className="botonCf">Enviar WhatsApp</button>
+            </div>
+            <div className="pasosCf">
+            <img src="./subir.png"></img>
+                <h3>SUBIR EQUIPO</h3>
+                <p>Cuando hables con nosotros, te vamos a pasar los datos para que puedas cargar tu equipo. Se hace Online.</p>
+                <button className="botonCf">Cargar Lista</button>
+            </div>
+            <div className="pasosCf">
+            <img src="./garrapata.png"></img>
+                <h3>LISTO</h3>
+                <p>Solo resta esperar hasta el primer partido. Todos los partidos figuran en el Fixture.</p>
+            </div>
           </div>
         </div>
     )
