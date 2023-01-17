cae53270896c2d40c85b36b737f502af269e07c3
AlvarezMariaBelen <beluchis1991@gmail.com>
2023-01-17 04:08:18 -0300

    agregado de logos de equipo

--- src/componentes/TablasPortada/TablasP.js
+++ src/componentes/TablasPortada/TablasP.js
@@ -24,7 +24,7 @@ export default function TablasP() {
             <tbody>
               <tr>
                 <th>Pos</th>
-                <th width="200">Equipo</th>
+                <th width="210">Equipo</th>
                 <th width="50">A</th>
                 <th width="50">J</th>
                 <th width="50">G</th>
@@ -36,7 +36,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>1</th>
-                <td>Bunker FC</td>
+                <td><img src="./bunker.png"></img>Bunker FC</td>
                 <td>-</td>
                 <td>9</td>
                 <td>8</td>
@@ -48,7 +48,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>2</th>
-                <td>Leicester +30</td>
+                <td><img src="./leicester.png"></img>Leicester +30</td>
                 <td>-</td>
                 <td>9</td>
                 <td>7</td>
@@ -60,7 +60,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>3</th>
-                <td>Los Bandidos</td>
+                <td><img src="./Falta-Escudo.png"></img>Los Bandidos</td>
                 <td>-</td>
                 <td>9</td>
                 <td>7</td>
@@ -72,7 +72,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>4</th>
-                <td>La Peste FG</td>
+                <td><img src="./lapeste.png"></img>La Peste FG</td>
                 <td>2</td>
                 <td>9</td>
                 <td>5</td>
@@ -84,7 +84,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>5</th>
-                <td>La Teoría</td>
+                <td><img src="./lateoria.png"></img>La Teoría</td>
                 <td>-</td>
                 <td>8</td>
                 <td>4</td>
@@ -96,7 +96,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>6</th>
-                <td>Once Cajas</td>
+                <td><img src="./oncecajas.png"></img>Once Cajas</td>
                 <td>-</td>
                 <td>9</td>
                 <td>4</td>
@@ -108,7 +108,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>7</th>
-                <td>Atlético La Peña</td>
+                <td><img src="./lapeña.png"></img>Atlético La Peña</td>
                 <td>-</td>
                 <td>9</td>
                 <td>2</td>
@@ -120,7 +120,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>8</th>
-                <td>Pucará</td>
+                <td><img src="./pucara.png"></img>Pucará</td>
                 <td>3</td>
                 <td>9</td>
                 <td>1</td>
@@ -132,7 +132,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>9</th>
-                <td>El Bajo</td>
+                <td><img src="./Falta-Escudo.png"></img>El Bajo</td>
                 <td>-</td>
                 <td>9</td>
                 <td>2</td>
@@ -144,7 +144,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>10</th>
-                <td>All Boys FC</td>
+                <td><img src="./allboys.png"></img>All Boys FC</td>
                 <td>-</td>
                 <td>9</td>
                 <td>0</td>
@@ -172,7 +172,7 @@ export default function TablasP() {
             <tbody>
               <tr>
                 <th>Pos</th>
-                <th width="200">Equipo</th>
+                <th width="210">Equipo</th>
                 <th width="50">A</th>
                 <th width="50">J</th>
                 <th width="50">G</th>
@@ -184,7 +184,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>1</th>
-                <td>Yapeyú FC</td>
+                <td><img src="./yapeyu.png"></img>Yapeyú FC</td>
                 <td>5</td>
                 <td>11</td>
                 <td>10</td>
@@ -196,7 +196,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>2</th>
-                <td>Niul</td>
+                <td><img src="./NIUL.png"></img>Niul</td>
                 <td>4</td>
                 <td>11</td>
                 <td>9</td>
@@ -208,7 +208,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>3</th>
-                <td>Firpo FC</td>
+                <td><img src="./firpofc.png"></img>Firpo FC</td>
                 <td>-</td>
                 <td>11</td>
                 <td>9</td>
@@ -220,7 +220,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>4</th>
-                <td>Peche Peye (M)</td>
+                <td><img src="./pechepeye.png"></img>Peche Peye (M)</td>
                 <td>-</td>
                 <td>11</td>
                 <td>8</td>
@@ -232,7 +232,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>5</th>
-                <td>Il Vicinato</td>
+                <td><img src="./ilvicinato.png"></img>Il Vicinato</td>
                 <td>-</td>
                 <td>11</td>
                 <td>6</td>
@@ -244,7 +244,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>6</th>
-                <td>Tallerismo T</td>
+                <td><img src="./tallerismo.png"></img>Tallerismo T</td>
                 <td>-</td>
                 <td>11</td>
                 <td>5</td>
@@ -256,7 +256,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>7</th>
-                <td>Los Chavales FC</td>
+                <td><img src="./chavales.png"></img>Los Chavales FC</td>
                 <td>-</td>
                 <td>11</td>
                 <td>4</td>
@@ -268,7 +268,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>8</th>
-                <td>Tallerismo A</td>
+                <td><img src="./tallerismo.png"></img>Tallerismo A</td>
                 <td>9</td>
                 <td>11</td>
                 <td>4</td>
@@ -280,7 +280,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>9</th>
-                <td>La Marcia</td>
+                <td><img src="./lamarcia.png"></img>La Marcia</td>
                 <td>-</td>
                 <td>11</td>
                 <td>3</td>
@@ -292,7 +292,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>10</th>
-                <td>Me Anotó Un Amigo FC</td>
+                <td><img src="./mauafc.png"></img>Me Anotó Un Amigo FC</td>
                 <td>-</td>
                 <td>11</td>
                 <td>3</td>
@@ -304,7 +304,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>11</th>
-                <td>Cuerito United</td>
+                <td><img src="./ESCUDO-CUERITO.png"></img>Cuerito United</td>
                 <td>-</td>
                 <td>11</td>
                 <td>2</td>
@@ -316,7 +316,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>12</th>
-                <td>Club Sportivo Provincianos</td>
+                <td><img src="./provinciano.png"></img>Club Sportivo Provincianos</td>
                 <td>-</td>
                 <td>11</td>
                 <td>1</td>
@@ -346,7 +346,7 @@ export default function TablasP() {
             <tbody>
               <tr>
                 <th><span>Pos</span></th>
-                <th width="200"><span>Equipo</span></th>
+                <th width="210"><span>Equipo</span></th>
                 <th width="50"><span>A</span></th>
                 <th width="50"><span>J</span></th>
                 <th width="50"><span>G</span></th>
@@ -358,7 +358,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>1</th>
-                <td>Sinergia</td>
+                <td><img src="./sinergia.jpg"></img>Sinergia</td>
                 <td>-</td>
                 <td>11</td>
                 <td>9</td>
@@ -370,7 +370,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>2</th>
-                <td>Peche Peye (F)</td>
+                <td><img src="./pechepeye.png"></img>Peche Peye (F)</td>
                 <td>-</td>
                 <td>11</td>
                 <td>8</td>
@@ -382,7 +382,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>3</th>
-                <td>Cara Interna</td>
+                <td><img src="./carainterna.png"></img>Cara Interna</td>
                 <td>-</td>
                 <td>11</td>
                 <td>6</td>
@@ -394,7 +394,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>4</th>
-                <td>Toro Viejo FC</td>
+                <td><img src="toroviejo.png"></img>Toro Viejo FC</td>
                 <td>-</td>
                 <td>10</td>
                 <td>4</td>
@@ -406,7 +406,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>5</th>
-                <td>Tararira</td>
+                <td><img src="./tararia.png"></img>Tararira</td>
                 <td>-</td>
                 <td>6</td>
                 <td>2</td>
@@ -418,7 +418,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>6</th>
-                <td>Picas Fem</td>
+                <td><img src="Picas-Fem.png"></img>Picas Fem</td>
                 <td>-</td>
                 <td>11</td>
                 <td>2</td>
@@ -430,7 +430,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>7</th>
-                <td>Pueblo FC</td>
+                <td><img src="./pueblofc.png"></img>Pueblo FC</td>
                 <td>-</td>
                 <td>6</td>
                 <td>1</td>
@@ -442,7 +442,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>8</th>
-                <td>Moverse Mas</td>
+                <td><img src="./moversemas.png"></img>Moverse Mas</td>
                 <td>-</td>
                 <td>11</td>
                 <td>1</td>
@@ -470,7 +470,7 @@ export default function TablasP() {
             <tbody>
               <tr>
                 <th>Pos</th>
-                <th width="200">Equipo</th>
+                <th width="210">Equipo</th>
                 <th width="50">A</th>
                 <th width="50">J</th>
                 <th width="50">G</th>
@@ -482,7 +482,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>1</th>
-                <td>Tamo Activo</td>
+                <td><img src="./tamoactivo.png"></img>Tamo Activo</td>
                 <td>-</td>
                 <td>8</td>
                 <td>7</td>
@@ -494,7 +494,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>2</th>
-                <td>La Prome</td>
+                <td><img src="./laprome.png"></img>La Prome</td>
                 <td>-</td>
                 <td>8</td>
                 <td>6</td>
@@ -506,7 +506,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>3</th>
-                <td>Avellaneda FC</td>
+                <td><img src="./avellaneda.jpeg"></img>Avellaneda FC</td>
                 <td>-</td>
                 <td>7</td>
                 <td>4</td>
@@ -518,7 +518,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>4</th>
-                <td>La Amparoneta</td>
+                <td><img src="./amparoneta.png"></img>La Amparoneta</td>
                 <td>-</td>
                 <td>7</td>
                 <td>2</td>
@@ -530,7 +530,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>5</th>
-                <td>Chakarita Jrs</td>
+                <td><img src="./chakarita.png"></img>Chakarita Jrs</td>
                 <td>12</td>
                 <td>7</td>
                 <td>3</td>
@@ -542,7 +542,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>6</th>
-                <td>Pura Quimica</td>
+                <td><img src="pura-quimica.png"></img>Pura Quimica</td>
                 <td>-</td>
                 <td>7</td>
                 <td>0</td>
@@ -572,7 +572,7 @@ export default function TablasP() {
             <tbody>
               <tr>
                 <th>Pos</th>
-                <th width="200">Equipo</th>
+                <th width="210">Equipo</th>
                 <th width="50">A</th>
                 <th width="50">J</th>
                 <th width="50">G</th>
@@ -584,7 +584,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>1</th>
-                <td>Pura Quimica</td>
+                <td><img src="./pura-quimica.png"></img>Pura Quimica</td>
                 <td>3</td>
                 <td>41</td>
                 <td>27</td>
@@ -596,7 +596,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>2</th>
-                <td>T-USA</td>
+                <td><img src="./tusa.png"></img>T-USA</td>
                 <td>-</td>
                 <td>41</td>
                 <td>21</td>
@@ -624,7 +624,7 @@ export default function TablasP() {
             <tbody>
               <tr>
                 <th>Pos</th>
-                <th width="200">Equipo</th>
+                <th width="210">Equipo</th>
                 <th width="50">A</th>
                 <th width="50">J</th>
                 <th width="50">G</th>
@@ -636,7 +636,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>1</th>
-                <td>La Peste FG</td>
+                <td><img src="./lapeste.png"></img>La Peste FG</td>
                 <td>3</td>
                 <td>13</td>
                 <td>11</td>
@@ -648,7 +648,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>2</th>
-                <td>Resa-K</td>
+                <td><img src="./resak.png"></img>Resa-K</td>
                 <td>3</td>
                 <td>13</td>
                 <td>9</td>
@@ -660,7 +660,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>3</th>
-                <td>La Granja</td>
+                <td><img src="./lagranjafc.png"></img>La Granja</td>
                 <td>-</td>
                 <td>13</td>
                 <td>8</td>
@@ -672,7 +672,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>4</th>
-                <td>Real Pueyrredon FC</td>
+                <td><img src="./realpuey.png"></img>Real Pueyrredon FC</td>
                 <td>3</td>
                 <td>13</td>
                 <td>6</td>
@@ -684,7 +684,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>5</th>
-                <td>Leicester</td>
+                <td><img src="./leicester.png"></img>Leicester</td>
                 <td>3</td>
                 <td>13</td>
                 <td>6</td>
@@ -696,7 +696,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>6</th>
-                <td>Celta de Vino</td>
+                <td><img src="./celtadevino.png"></img>Celta de Vino</td>
                 <td>-</td>
                 <td>13</td>
                 <td>7</td>
@@ -708,7 +708,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>7</th>
-                <td>Galácticos FC</td>
+                <td><img src="./galacticos.jpeg"></img>Galácticos FC</td>
                 <td>7</td>
                 <td>13</td>
                 <td>9</td>
@@ -720,7 +720,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>8</th>
-                <td>Deportivo La Tala</td>
+                <td><img src="./escudodeportivolatala.png"></img>Deportivo La Tala</td>
                 <td>3</td>
                 <td>13</td>
                 <td>6</td>
@@ -732,7 +732,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>9</th>
-                <td>La Teoría</td>
+                <td><img src="./lateoria.png"></img>La Teoría</td>
                 <td>3</td>
                 <td>12</td>
                 <td>4</td>
@@ -744,7 +744,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>10</th>
-                <td>El Bajo (LP)</td>
+                <td><img src="./elbajo.png"></img>El Bajo (LP)</td>
                 <td>-</td>
                 <td>12</td>
                 <td>3</td>
@@ -756,7 +756,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>11</th>
-                <td>Imprevisto</td>
+                <td><img src="./imprevisto.png"></img>Imprevisto</td>
                 <td>4</td>
                 <td>13</td>
                 <td>2</td>
@@ -768,7 +768,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>12</th>
-                <td>Les Blues AD</td>
+                <td><img src="./pura-quimica.png"></img>Les Blues AD</td>
                 <td>-</td>
                 <td>13</td>
                 <td>8</td>
@@ -780,7 +780,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>13</th>
-                <td>La Escabieta</td>
+                <td><img src="./escabieta.png"></img>La Escabieta</td>
                 <td>-</td>
                 <td>13</td>
                 <td>2</td>
@@ -792,7 +792,7 @@ export default function TablasP() {
               </tr>
               <tr>
                 <th>14</th>
-                <td>Toronto FC</td>
+                <td><img src="./toronto.png"></img>Toronto FC</td>
                 <td>-</td>
                 <td>13</td>
                 <td>2</td>
